1f4cbb2d08565825577fdce02a33a5a16be2199e
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-04-10 17:59:49 +0200

    Assignment1

---
+++ DP-Week1/DP-Week1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DP_Week1_Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.start();
+        }
+        void start()
+        {
+
+        }
+    }
+}
