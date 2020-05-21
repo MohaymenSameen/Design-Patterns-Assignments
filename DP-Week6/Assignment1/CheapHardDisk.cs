9b93e57a1c0e34fcf307e6001c53047c6f041ab4
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-05-21 23:22:38 +0200

    added some code and classes

---
+++ DP-Week6/Assignment1/CheapHardDisk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public class CheapHardDisk : IHardDisk
+    {
+        public void StoreData()
+        {
+            Console.WriteLine("Storing data not so quickly...");
+        }
+    }
+}
---
+++ DP-Week6/Assignment1/CheapMonitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    public class CheapMonitor : IMonitor
+    {
+        public void Display()
+        {
+            Console.WriteLine("displaying stuff very poor...");
+        }
+    }
+}
