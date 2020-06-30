a394d0728786284dea8c7970e8dfc5328499adb5
MohaymenSameen <MohaymenSameen@users.noreply.github.com>
2020-07-01 00:24:47 +0200

    Assignment 3 done

---
+++ DP-Practice -Exam/Assignment3/CopyingMachine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment3
+{
+    public class CopyingMachine
+    {
+        private int totalNumberOfCopies;
+        public int TotalNumberOfCopies { get { return totalNumberOfCopies; } set { } }
+        private static CopyingMachine uniqueInstance;
+
+        public CopyingMachine()
+        {
+            totalNumberOfCopies = 0;
+        }
+        public void Copy(int copies)
+        {
+            Console.WriteLine("copying,{0}x", copies);
+            totalNumberOfCopies += copies;
+            Console.WriteLine("total number of copies: {0}", totalNumberOfCopies);            
+        }
+        public static CopyingMachine GetInstance()
+        {
+            if(uniqueInstance == null)
+            {
+                uniqueInstance = new CopyingMachine();
+            }
+            return uniqueInstance;
+        }
+    }
+}
---
+++ DP-Practice -Exam/Assignment3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myprogram = new Program();
+            myprogram.Start();
+        }
+        void Start()
+        {
+            CopyingMachine copyingMachine1 = CopyingMachine.GetInstance();
+            CopyingMachine copyingMachine2 = CopyingMachine.GetInstance();
+
+            Console.WriteLine("copying with 'machine 1'");
+            copyingMachine1.Copy(10);
+            copyingMachine1.Copy(23);
+
+            Console.WriteLine("copying with 'machine 2'");
+            copyingMachine2.Copy(40);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
